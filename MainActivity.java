e0f18124510f205350dc2355ee3421d56c43f66a
isabellarw <61801180+isabellarw@users.noreply.github.com>
2020-05-01 22:51:27 -0400

    Add files via upload
    Basic practice creating the button which user can press to mark a bingo space.

--- MainActivity.java
+++ MainActivity.java
@@ -1,40 +1,34 @@
-package com.example.welcome;
+package com.example.bingo;
 
+import android.view.View;
+import android.view.Window;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
-import android.view.Window;
-//import android.view.WindowManager; used for fullscreen
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
-
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getSupportActionBar().hide(); //get rid of upper bar
-//        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN); make fullscreen
         setContentView(R.layout.activity_main);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
 
-        Button addBtn = (Button) findViewById(R.id.addBtn);
-        addBtn.setOnClickListener(new View.OnClickListener() {
+        ImageButton imageButton = (ImageButton) findViewById(R.id.imageButton);
+        imageButton.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View v) {
-                EditText firstNumEditText = (EditText) findViewById(R.id.firstNumEditText);
-                EditText secondNumEditText = (EditText) findViewById(R.id.secondNumEditText);
-                TextView resultTextView = (TextView) findViewById(R.id.resultTextView);
+            public void onClick(View v){
+                TextView space = (TextView) findViewById(R.id.space);
+                space.setText('X');
 
-                int num1 = Integer.parseInt(firstNumEditText.getText().toString());
-                int num2 = Integer.parseInt(secondNumEditText.getText().toString());
-                int result = num1 + num2;
-                resultTextView.setText(result + "");
             }
+
         });
+
 
     }
 }
